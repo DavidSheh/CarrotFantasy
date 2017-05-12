918d0228e58b4916840ce539f9693edcebb9e335
sheweiwei <sheweiwei@hotcast.cn>
2017-05-12 19:13:50 +0800

    [add] add Map Editor

---
+++ Assets/Game/Scripts/Application/Consts.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public static class Consts
+{
+    //目录
+    public static readonly string LevelDir = Application.dataPath + @"\Game\Res\Levels";
+    public static readonly string MapDir = Application.dataPath + @"\Game\Res\Maps";
+
+    //存档
+
+
+    //Model
+
+
+    //View
+
+
+    //Controller
+
+}
+
